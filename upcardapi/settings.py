82ef331ac0376996785e8bc9a6242408ddb35fdb
olega211 <106370448+olega211@users.noreply.github.com>
2023-07-18 21:06:48 +0700

    debug off

--- upcardapi/settings.py
+++ upcardapi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5#5&*&k0f31$8xg%u53667q=!bxi+sh(4e)q+#25^ac)23*5@n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['upcard.online', '127.0.0.1']
 
